b8e05e376fc086c0cd5dc861d112a881b86e746b
Atlas <99806895+AtlasMountains@users.noreply.github.com>
2022-06-28 11:49:21 +0200

    basic user model & table

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,11 +15,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+         \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+         \App\Models\User::factory()->create([
+             'name' => 'admin',
+             'email' => 'admin@mail.com',
+             'email_verified_at' => now(),
+             'is_admin' => true,
+             'password' => Hash::make('admin'),
+         ]);
     }
 }
