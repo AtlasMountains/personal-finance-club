447b12628e6b1ca0ad0bcb89697ef61f33eb06a9
Atlas <99806895+AtlasMountains@users.noreply.github.com>
2022-07-07 01:15:59 +0200

    livewire transactions table

---
+++ app/Http/Livewire/Account.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class Account extends Component
+{
+    public function render()
+    {
+        return view('livewire.account');
+    }
+}
