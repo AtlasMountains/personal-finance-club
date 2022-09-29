40d5e6f07115641500c0dba1cc4ea57a0826d58c
Atlas <99806895+AtlasMountains@users.noreply.github.com>
2022-09-30 00:47:17 +0200

    sending register emails using queuing

--- app/Http/Controllers/auth/UserRegisterController.php
+++ app/Http/Controllers/auth/UserRegisterController.php
@@ -4,8 +4,8 @@ namespace App\Http\Controllers\auth;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\UserCreateRequest;
+use App\Jobs\RegisterUser;
 use App\Models\User;
-use Illuminate\Auth\Events\Registered;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
@@ -20,7 +20,7 @@ class UserRegisterController extends Controller
 
     public function index(Request $request): Factory|View|Application
     {
-        $key = 'register.'.$request->ip();
+        $key = 'register.' . $request->ip();
         $remaining = RateLimiter::retriesLeft($key, $this->maxAttempts);
         $wait_time = now()->addSeconds(RateLimiter::availableIn($key))->diffForHumans();
 
@@ -29,7 +29,7 @@ class UserRegisterController extends Controller
 
     public function store(UserCreateRequest $request): RedirectResponse
     {
-        $key = 'register.'.$request->ip();
+        $key = 'register.' . $request->ip();
         if (RateLimiter::tooManyAttempts($key, $this->maxAttempts)) {
             return back()->withErrors(['max_attempts' => trans('auth.attempts_max')]);
         }
@@ -40,8 +40,11 @@ class UserRegisterController extends Controller
             'email' => $request->email,
             'password' => Hash::make($request->password),
         ]);
-//        send email
-        event(new Registered($user));
+//        send email using jobs and queing
+        RegisterUser::dispatch($user);
+//       old code
+//       event(new Registered($user));
+
 //        limit registrations by ip decay per 1 hours
         RateLimiter::hit($key, $this->maxAttempts * 3600);
         auth()->attempt($request->only('email', 'password'));
